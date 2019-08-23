1dd35231ebdceff0f6b818beaed62f8fe60e7e40
SoumyaDudka <54269596+SoumyaDudka@users.noreply.github.com>
2019-08-23 15:26:54 +0530

    Update RunCukesTest.java

--- src/test/java/skeleton/RunCukesTest.java
+++ src/test/java/skeleton/RunCukesTest.java
@@ -6,11 +6,10 @@ import cucumber.api.CucumberOptions;
 import cucumber.api.junit.Cucumber;
 
 @RunWith(Cucumber.class)
-@CucumberOptions(features="CS_Feature",
-glue = "caseStudy_TestMe",tags = {"@Sanity"},
+@CucumberOptions(tags = {"@Sanity"},
 plugin = {"html:target/cucumber-htmlreport.html","json:target/cucumber.json"})
 
 
-public class runner_case {
+public class RunCukesTest {
 
 }
